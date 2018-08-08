ad845112603c145808f6172c53efc757ec176326
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-08-08 14:43:08 -0700

    scaling and offsetting are distinguished

--- src/basePitch.ts
+++ src/basePitch.ts
@@ -1,4 +1,4 @@
-import { Frequency } from '../../../src/types'
+import { Frequency } from '../../../src/utilities/nominalTypes'
 
 // tslint:disable-next-line:no-any no-magic-numbers
 const C8_FOR_STEPWISE: Frequency = 4186 as any
