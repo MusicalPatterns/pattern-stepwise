38912659a86fdcc1986321dfb073cc50d9ea9a01
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-10-12 22:32:52 -0700

    separate song metadata from song config and song itself

---
+++ src/metadata.ts
@@ -0,0 +1,11 @@
+import { SongID, SongMetadata } from '../../../src/songTypes'
+
+const stepwiseSongMetadata: SongMetadata = {
+    description: 'stepwise motion within recurring bounds',
+    formattedName: 'Stepwise',
+    songId: SongID.STEPWISE,
+}
+
+export {
+    stepwiseSongMetadata,
+}
--- src/songs.ts
+++ src/songs.ts
@@ -1,7 +1,7 @@
 import { defaultSongCompile } from '../../../src/compile/defaultSongCompile'
 import { EntityConfigs } from '../../../src/compile/types'
 import { subharmonicSeriesPitches } from '../../../src/pitches'
-import { Song, SongID } from '../../../src/songTypes'
+import { SongConfig, SongID } from '../../../src/songTypes'
 import sequence from '../../../src/utilities/sequence'
 import * as to from '../../../src/utilities/to'
 import { C8_FOR_STEPWISE } from './basePitch'
@@ -47,25 +47,23 @@ const stepwisePercussion: EntityConfigs = [
     stepwiseHihat,
 ]
 
-const stepwise: Song = {
+const stepwiseSongConfig: SongConfig = {
     compile: defaultSongCompile,
     config: {
         baseFrequency: C8_FOR_STEPWISE,
         durationScalar: to.Scalar(1),
     },
-    description: 'stepwise motion within recurring bounds',
     entityConfigs: sequence([
         stepwiseSamples,
         // stepwiseOscillatorsForCheckingTuningAgainst,
         // stepwisePercussion,
     ]),
-    formattedName: 'Stepwise',
-    id: SongID.STEPWISE,
     scales: [
         subharmonicSeriesPitches,
     ],
+    songId: SongID.STEPWISE,
 }
 
 export {
-    stepwise,
+    stepwiseSongConfig,
 }
