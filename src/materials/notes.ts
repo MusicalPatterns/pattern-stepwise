ee4bb6557eb34ecbed52a1dd94e8b085fcc45a77
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-27 13:06:27 -0800

    compiler now manages sustain defaulting to close to duration

--- src/materials/notes.ts
+++ src/materials/notes.ts
@@ -1,20 +1,11 @@
 import { NoteSpec } from '@musical-patterns/compiler'
-import {
-    DEFAULT_DURATIONS_SCALE_INDEX,
-    DEFAULT_OFFSET_FOR_ALMOST_FULL_SUSTAIN,
-    DEFAULT_PITCH_SCALE_INDEX,
-} from '@musical-patterns/pattern'
+import { DEFAULT_DURATIONS_SCALE_INDEX, DEFAULT_PITCH_SCALE_INDEX } from '@musical-patterns/pattern'
 import { to } from '@musical-patterns/utilities'
 
 const buildUnpitchedNoteSpec: (duration: number) => NoteSpec =
     (duration: number): NoteSpec => ({
         durationSpec: {
             index: to.Index(duration),
-            scaleIndex: DEFAULT_DURATIONS_SCALE_INDEX,
-        },
-        sustainSpec: {
-            index: to.Index(duration),
-            offset: DEFAULT_OFFSET_FOR_ALMOST_FULL_SUSTAIN,
             scaleIndex: DEFAULT_DURATIONS_SCALE_INDEX,
         },
     })
