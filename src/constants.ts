45c0e10c2c233227fe60bc3b257018193080abf5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-05 10:58:19 -0800

    1.0.73: scientific pitch

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,16 @@
 // tslint:disable no-magic-numbers
 
-import { Frequency, Milliseconds, to } from '@musical-patterns/utilities'
+import {
+    Frequency,
+    Milliseconds,
+    SCIENTIFIC_PITCHES,
+    ScientificPitchNoteName,
+    ScientificPitchOctaveNumber,
+    to,
+} from '@musical-patterns/utilities'
 
-const C8: Frequency = to.Frequency(4186)
-const STEPWISE_INITIAL_BASE_FREQUENCY: Frequency = C8
+const STEPWISE_INITIAL_BASE_FREQUENCY: Frequency =
+    SCIENTIFIC_PITCHES[ ScientificPitchNoteName.C ][ ScientificPitchOctaveNumber._8 ]
 const STEPWISE_INITIAL_BASE_DURATION: Milliseconds = to.Milliseconds(100)
 
 export {
