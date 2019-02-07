0094ea46df4b61536e2f7a2cbf3aebed92ad4dca
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-07 14:26:29 -0800

    1.0.77: nominal constants, and rounded snapshot

--- src/material/scales.ts
+++ src/material/scales.ts
@@ -1,6 +1,6 @@
 import { BuildScalesFunction, Scale } from '@musical-patterns/compiler'
 import { buildStandardScales, StandardSpec, StandardSpecProperties } from '@musical-patterns/pattern'
-import { from, to } from '@musical-patterns/utilities'
+import { from, NO_TRANSLATION, to } from '@musical-patterns/utilities'
 
 const buildScales: BuildScalesFunction =
     (spec: StandardSpec): Scale[] => {
@@ -10,12 +10,12 @@ const buildScales: BuildScalesFunction =
         const durationsScale: Scale = {
             scalar: to.Scalar(from.Milliseconds(spec[ StandardSpecProperties.BASE_DURATION ] || to.Milliseconds(1))),
             scalars: flatDurationsScale.scalars,
-            translation: spec[ StandardSpecProperties.DURATION_TRANSLATION ] || to.Translation(0),
+            translation: spec[ StandardSpecProperties.DURATION_TRANSLATION ] || NO_TRANSLATION,
         }
         const pitchesScale: Scale = {
             scalar: to.Scalar(from.Frequency(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Frequency(1))),
             scalars: subharmonicSeriesScale.scalars,
-            translation: spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || to.Translation(0),
+            translation: spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || NO_TRANSLATION,
         }
 
         return [
