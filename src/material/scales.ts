93acf4cfcda0cf77da569861ce70953d0a9ed2aa
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-25 10:18:29 -0800

    1.0.85: use standard scales

--- src/material/scales.ts
+++ src/material/scales.ts
@@ -1,37 +1,20 @@
 import { BuildScalesFunction, Scale } from '@musical-patterns/compiler'
-import { buildStandardScales, StandardSpec, StandardSpecProperties } from '@musical-patterns/pattern'
-import { from, NO_TRANSLATION, Scalar, to, Translation } from '@musical-patterns/utilities'
+import {
+    buildFlatDurationsScale,
+    buildStandardScales,
+    buildSubharmonicSeriesScale,
+    StandardSpec,
+} from '@musical-patterns/pattern'
 
 const buildScales: BuildScalesFunction =
-    (spec: StandardSpec): Scale[] => {
-        const { nonScale, flatDurationsScale, subharmonicSeriesScale } = buildStandardScales()
-
-        const gainScale: Scale = nonScale
-        const durationScalar: Scalar =
-            from.Ms(spec[ StandardSpecProperties.BASE_DURATION ] || to.Scalar(to.Ms(1)))
-        const durationTranslation: Translation =
-            from.Ms(spec[ StandardSpecProperties.DURATION_TRANSLATION ] || to.Ms(NO_TRANSLATION))
-        const durationsScale: Scale = {
-            scalar: durationScalar,
-            scalars: flatDurationsScale.scalars,
-            translation: durationTranslation,
-        }
-        const pitchesScalar: Scalar =
-            from.Hz(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Scalar(to.Hz(1)))
-        const pitchesTranslation: Translation =
-            from.Hz(spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || to.Hz(NO_TRANSLATION))
-        const pitchesScale: Scale = {
-            scalar: pitchesScalar,
-            scalars: subharmonicSeriesScale.scalars,
-            translation: pitchesTranslation,
-        }
-
-        return [
-            gainScale,
-            durationsScale,
-            pitchesScale,
-        ]
-    }
+    (spec: StandardSpec): Scale[] =>
+        buildStandardScales(
+            spec,
+            {
+                durationScalars: buildFlatDurationsScale().scalars,
+                pitchScalars: buildSubharmonicSeriesScale().scalars,
+            },
+        )
 
 export {
     buildScales,
