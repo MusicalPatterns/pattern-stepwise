bc7cd09225d09f1acde2605104a6d7f62384a05c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-29 21:14:49 -0700

    1.0.108: exclude snapshot from package

--- src/indexForTest.ts
+++ src/indexForTest.ts
@@ -9,8 +9,3 @@ export {
     StepwiseBlocks,
 } from './material/indexForTest'
 export { pattern } from './patterns'
-
-// tslint:disable-next-line no-default-import
-import * as snapshot from './snapshot.json'
-
-export { snapshot }
