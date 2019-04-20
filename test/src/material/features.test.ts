75316a08be002284cbe6dac529f37ee9ab1c2025
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-19 17:05:00 -0700

    1.0.122: nominal type refinements

--- test/src/material/features.test.ts
+++ test/src/material/features.test.ts
@@ -1,5 +1,5 @@
 import { Note } from '@musical-patterns/material'
-import { to } from '@musical-patterns/utilities'
+import { Scalar, to } from '@musical-patterns/utilities'
 import { computeContourElement, computeNote } from '../../../src/indexForTest'
 
 describe('features', () => {
@@ -7,6 +7,6 @@ describe('features', () => {
         const note: Note = computeNote(computeContourElement(3))
 
         expect(note.duration!.index)
-            .toBe(to.Ordinal(2))
+            .toBe(to.Ordinal<Scalar>(2))
     })
 })
