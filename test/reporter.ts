efb41322f53a9b6f36daa4a3b794476bc5a49d46
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-11-27 19:39:55 -0800

    1.0.147: rm cached

--- test/reporter.ts
+++
@@ -1,8 +0,0 @@
-import { SpecReporter } from 'jasmine-spec-reporter'
-
-Error.stackTraceLimit = Infinity
-
-jasmine.getEnv()
-    .clearReporters()
-jasmine.getEnv()
-    .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }))
